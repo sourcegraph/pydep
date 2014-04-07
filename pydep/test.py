ac9c605d9e6723f35c241e164d9461547f6d4245
Beyang Liu <beyang.liu@gmail.com>
2014-04-07 16:00:57 -0700

    test placeholder

---
+++ pydep/test.py
@@ -0,0 +1,6 @@
+# TODO(a placeholder for tests):
+# - requirements.txt present
+# - no requirements.txt present, setup.py present
+# - no requirements.txt or setup.py - graceful failure
+# - requirement doesn't have setup.py - best effort with warning
+# - requirement setup.py reads relative file path (see package 'invoke')
